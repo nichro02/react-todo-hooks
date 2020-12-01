261646c31df79755c685540e778ca109a7ec979c
mateenkazia <mateen.kazia@chaione.com>
2020-11-30 19:55:54 -0800

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Components Import
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+// CSS import
+import "../css/components/TodoList.css";
+
+/*
+  TodoMVC ✔️
+  1. add todo 
+  2. display todos 
+  3. cross off todo  
+  4. show number of active todos 
+  5. delete todo 
+  <--- EXTRA CREDIT 👇 ---> 
+  6. filter all/active/complete 
+  7. delete all complete
+    7.1 only show if atleast one is complete 
+  8. button to toggle all on/off 
+
+  https://appian-mock.herokuapp.com/todos
+*/
+
+const TodoList = () => {
+  return (
+    <div>
+      todoList...
+    </div>
+  );
+};
+
+export default TodoList;
